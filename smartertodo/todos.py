002bba2a192e2d3f258e5a715cb7b638315a23d1
ianertson <sebbekarlsson97@gmail.com>
2018-12-12 12:59:42 +0100

    removed unused file

--- smartertodo/todos.py
+++
@@ -1,8 +0,0 @@
-from smartertodo.comments import get_comments_from_directory
-
-
-def get_todos_from_directory(dirname):
-    return filter(
-        lambda x: 'TODO' in x.value,
-        get_comments_from_directory(dirname)
-    )
